8e646234112472a34f372fc787676364536705c4
Austin Luing <wildfan46@gmail.com>
2026-02-27 22:23:45 -0600

    chore: hardcoding cors headers so I can go to bed

--- src/app.py
+++ src/app.py
@@ -38,5 +38,10 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "https://www.austinluing.com",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+        },
         "body": json.dumps(response_body)
     }
